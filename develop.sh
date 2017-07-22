ba26c20473f0ad34638ed1fac3f0975e35b6495f
Chris Kim <christopherjkim@gmail.com>
2017-07-21 22:18:11 -0400

    thing that doesnt work

--- develop.sh
+++ develop.sh
@@ -4,3 +4,11 @@
 
 # builds and runs whenever a gwyfpatcher/*.cs file changes
 nodemon --watch gwyfpatcher -e cs --exec "msbuild && cd gwyfpatcher/bin/Debug && mono gwyfpatcher.exe"
+
+# mv gwyfpatcher/bin/Assembly-CSharp.dll gwyfpatcher/bin/Assembly-CSharp.original.dll
+# mono packages/ILRepack.2.0.13/tools/ILRepack.exe /targetplatform:v2 /lib:gwyfpatcher/bin /out:gwyfpatcher/bin/Assembly-CSharp.dll gwyfpatcher/bin/Assembly-CSharp.original.dll deps/unity-python/IronPython-2.6.2/*.dll
+
+# subl ~/Library/Logs/Unity/Player.log
+# or on windows:
+# _EXECNAME_Data_\output_log.txt
+# %USERPROFILE%\AppData\LocalLow\CompanyName\ProductName\output_log.txt
--- gwyfpatcher/Program.cs
+++ gwyfpatcher/Program.cs
@@ -28,12 +28,13 @@ namespace gwyfpatcher
 
             MethodDef method = new MethodDefUser(
                 methodName,
-                MethodSig.CreateStatic(mod.CorLibTypes.Void),
+                MethodSig.CreateInstance(mod.CorLibTypes.Void),
                 MethodImplAttributes.IL | MethodImplAttributes.Managed,
                 MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.ReuseSlot
             );
 
             klass.Methods.Add(method);
+            method.Body = new CilBody();
             return method;
         }
 
@@ -46,16 +47,114 @@ namespace gwyfpatcher
                 Console.WriteLine("fuk");
             }
 
-            //method.Body.Instructions.Insert(0, OpCodes.Ldarg_0.ToInstruction());
-            method.Body.Instructions.Insert(0, OpCodes.Call.ToInstruction(newMethod));
+            method.Body.Instructions.Insert(0, OpCodes.Ldarg_0.ToInstruction());
+            method.Body.Instructions.Insert(1, OpCodes.Call.ToInstruction(newMethod));
 
             return method;
+        }
+
+        public static CilBody PreStartBody(ModuleDefMD mod)
+        {
+            CilBody body = new CilBody();
+            var i = body.Instructions;
+
+            TypeDef ScriptScopeTypeDef = mod.Find("Microsoft.Scripting.Hosting.ScriptScope", false);
+            TypeDef ScriptSourceTypeDef = mod.Find("Microsoft.Scripting.Hosting.ScriptSource", false);
+            TypeDef ScriptEngineTypeDef = mod.Find("Microsoft.Scripting.Hosting.ScriptEngine", false);
+            TypeDef PythonTypeDef = mod.Find("IronPython.Hosting.Python", false);
+
+            AssemblyRef UnityEngineRef = mod.GetAssemblyRef("UnityEngine");
+            TypeRefUser DebugTypeRef = new TypeRefUser(mod, "UnityEngine", "Debug", UnityEngineRef);
+
+            TypeSig ScriptEngineTypeSig = ScriptEngineTypeDef.ToTypeSig();
+            TypeSig ScriptScopeTypeSig = ScriptScopeTypeDef.ToTypeSig();
+            TypeSig ScriptSourceTypeSig = ScriptSourceTypeDef.ToTypeSig();
+
+            MemberRef CreateEngineRef = new MemberRefUser(mod, "CreateEngine",
+                MethodSig.CreateStatic(ScriptEngineTypeSig), PythonTypeDef);
+
+            MemberRef CreateScopeRef = new MemberRefUser(mod, "CreateScope",
+                MethodSig.CreateInstance(ScriptScopeTypeSig), ScriptEngineTypeDef);
+
+            MemberRef CreateScriptSourceFromStringRef = new MemberRefUser(mod, "CreateScriptSourceFromString",
+                MethodSig.CreateInstance(ScriptSourceTypeSig, mod.CorLibTypes.String), ScriptEngineTypeDef);
+
+            MemberRef ExecuteRef = new MemberRefUser(mod, "Execute",
+                MethodSig.CreateInstance(mod.CorLibTypes.Object, ScriptScopeTypeSig), ScriptSourceTypeDef);
+
+            // MemberRef GetVariableRef = new MemberRefUser(mod, "GetVariable",
+            //     MethodSig.CreateInstance(mod.CorLibTypes.String, mod.CorLibTypes.String), ScriptScopeTypeDef);
+
+            MemberRef GetVariableRef = new MemberRefUser(mod, "GetVariable",
+                MethodSig.CreateInstanceGeneric(1, mod.CorLibTypes.String, mod.CorLibTypes.String), ScriptScopeTypeDef);
+
+            // MethodDef GetVariableDef = PythonTypeDef.FindMethod("GetVariable",
+            //     MethodSig.CreateInstance(mod.CorLibTypes.String, mod.CorLibTypes.String));
+
+            MemberRef LogRef = new MemberRefUser(mod, "LogError",
+                MethodSig.CreateStatic(mod.CorLibTypes.Void, mod.CorLibTypes.String), DebugTypeRef);
+
+
+            /*
+                ScriptEngine scriptEngine = Python.CreateEngine();
+                ScriptScope ScriptScope = scriptEngine.CreateScope();
+            */
+            Local local0 = new Local(ScriptScopeTypeSig);
+            body.Variables.Add(local0);
+
+            i.Add(OpCodes.Call.ToInstruction(CreateEngineRef));
+            i.Add(OpCodes.Dup.ToInstruction());
+            i.Add(OpCodes.Callvirt.ToInstruction(CreateScopeRef));
+            i.Add(OpCodes.Stloc.ToInstruction(local0));
+
+            /*
+                string example = "output = 'hello world'";
+            */
+            Local local1 = new Local(mod.CorLibTypes.String);
+            body.Variables.Add(local1);
+
+            i.Add(OpCodes.Ldstr.ToInstruction("output = 'hello world'"));
+            i.Add(OpCodes.Stloc.ToInstruction(local1));
+            i.Add(OpCodes.Ldloc.ToInstruction(local1));
+
+            /*
+                scriptEngine.CreateScriptSourceFromString(example).Execute(ScriptScope);
+            */
+            i.Add(OpCodes.Callvirt.ToInstruction(CreateScriptSourceFromStringRef));
+            i.Add(OpCodes.Ldloc.ToInstruction(local0));
+            i.Add(OpCodes.Callvirt.ToInstruction(ExecuteRef));
+            i.Add(OpCodes.Pop.ToInstruction());
+
+            /*
+                UnityEngine.Debug.Log(ScriptScope.GetVariable<string>("output"));
+            */
+            // i.Add(OpCodes.Ldloc.ToInstruction(local0));
+            // i.Add(OpCodes.Ldstr.ToInstruction("output"));
+            // i.Add(OpCodes.Callvirt.ToInstruction(GetVariableRef));
+            // i.Add(OpCodes.Call.ToInstruction(LogRef));
+
+            /*
+                UnityEngine.Debug.Log("hello");
+            */
+            i.Add(OpCodes.Ldstr.ToInstruction("hello"));
+            i.Add(OpCodes.Call.ToInstruction(LogRef));
+
+            i.Add(OpCodes.Ret.ToInstruction());
+            return body;
         }
 
         public static void Main(string[] args)
         {
             ModuleDefMD mod = ModuleDefMD.Load(GetModulePath());
+
+            MethodDef PreStart = AddMethod(mod, "BallMovement", "PreStart");
+            PreStart.Body = PreStartBody(mod);
+
             MethodDef preUpdate = AddMethod(mod, "BallMovement", "PreUpdate");
+
+            preUpdate.Body.Instructions.Add(OpCodes.Ret.ToInstruction());
+
+            PrependMethod(mod, "BallMovement", "Start", PreStart);
             PrependMethod(mod, "BallMovement", "Update", preUpdate);
 
             mod.Write(GetOutputModulePath());
